89d544ddb7e9558493c939656e3b2949be814772
Emuraye Pius 0 <76441493+oemuraye@users.noreply.github.com>
2022-05-10 21:33:35 +0100

    init

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// const dropdown = document.querySelector('.dropdownMenuLink')
+// const dropdownArrow = document.querySelectorAll(".dropdown-arrow");
+
+
+// //hamburger toggle
+// dropdown.addEventListener("click", () => {
+//     dropdownArrow.classList.remove("fa-angle-down");
+//     dropdownArrow.classList.add("fa-angle-up");
+
+//   if (dropdown.classList.contains("show")) {
+//     dropdownArrow.classList.remove("fa-angle-up");
+//     dropdownArrow.classList.add("fa-angle-down");
+// }
+// });
+
