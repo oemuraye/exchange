1bf35985980f8e2fb109b8fcc25b391775155f08
Emuraye Pius 0 <76441493+oemuraye@users.noreply.github.com>
2022-05-12 10:29:53 +0100

    added sections and did stylings

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,28 @@
 // }
 // });
 
+
+
+//  the modal
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.querySelector('.close-button')
+
+// When the user clicks the button, open the modal 
+btn.onclick = () => {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = () => {
+    console.log(span);
+    modal.style.display = "none";
+    console.log(span);
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = (e) => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
