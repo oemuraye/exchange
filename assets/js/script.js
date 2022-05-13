ce3e873105502163cbd1bd7c783ab90952daad99
Emuraye Pius 0 <76441493+oemuraye@users.noreply.github.com>
2022-05-13 11:44:57 +0100

    added sections and added styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,25 @@
 // });
 
 
+// Navbar background change 
+const myNav = document.querySelector(".header-navbar");
+
+window.onscroll = () => {
+  "use strict";
+  if (
+    document.body.scrollTop >= 400 ||
+    document.documentElement.scrollTop >= 75
+  ) {
+    myNav.classList.add("navbar-color-onscroll");
+    myNav.classList.remove("navbar-default-color");
+    backToTopBtn.style.display = "block";
+  } else {
+    myNav.classList.add("navbar-default-color");
+    myNav.classList.remove("navbar-color-onscroll");
+    backToTopBtn.style.display = "none";
+  }
+};
+
 
 //  the modal
 var modal = document.getElementById("myModal");
@@ -38,3 +57,10 @@ window.onclick = (e) => {
     modal.style.display = "none";
   }
 }
+
+var popoverTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+);
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
